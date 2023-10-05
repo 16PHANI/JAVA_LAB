7af36c2c7325f2e5c39412b0a552c83121e6438e
Boyinapalli Phani Shankar <113121127+16PHANI@users.noreply.github.com>
2023-10-05 23:55:07 +0530

    Tester.java

--- Tester.java
+++ Tester.java
@@ -20,3 +20,34 @@ public class Tester
         obj.close();
     }
 }
+
+
+//this code is used in Tester.java for performing Arthemitic operations.so save it as Utility.java in seperate file 
+public class Utility
+{
+    public static int  addNumbers(int n1,int n2)
+    {
+        return n1+n2;
+    }
+    public static int diffNumbers(int n1,int n2)
+    {
+        return n1-n2;
+    }
+    public static int proNumbers(int n1,int n2)
+    {
+        return n1*n2;
+    }
+    public static int diviNumbers(int n1,int n2)
+    {
+        if (n2!=0)
+        {
+            return n1/n2;
+        }
+        else
+        {
+            System.out.println("Check your Input : ");
+            return -1;
+        }
+    }
+
+}
